a83249906d36b936c72425954539e20423b9aa29
rbresearch <rossbennett34@gmail.com>
2012-12-27 18:17:03 -0600

    revised post per feedback

--- src/2012-12-25-logical-vector-count_if.cpp
+++ src/2012-12-25-logical-vector-count_if.cpp
@@ -2,7 +2,7 @@
  * @title Using LogicalVector
  * @author Ross Bennett
  * @license GPL (>= 2)
- * @tags sugar
+ * @tags sugar stl
  * @summary Illustrates the use of logical vectors
  */
 
@@ -13,8 +13,8 @@
  * one to easily write simple and expressive functions with
  * a LogicalVector as an argument.
  * Any of the logical operators `<, <=, >, >=, ==, !=` can be used
- * to create a logical sugar expression. The Rcpp sugar documentation
- * has additional examples of using binary logical operators.
+ * to create a logical sugar expression. The [Rcpp sugar](http://cran.r-project.org/web/packages/Rcpp/vignettes/Rcpp-sugar.pdf)
+ * vignette has additional examples of using binary logical operators.
  */
 
 #include <Rcpp.h>
@@ -22,13 +22,13 @@ using namespace Rcpp;
  
 // [[Rcpp::export]]
 int count_if(LogicalVector x) {
-	int counter = 0;
-	for(int i = 0; i < x.size(); i++) {
-		if(x[i] == TRUE) {
-			counter += 1;
-		}
-	}
-	return counter;
+    int counter = 0;
+    for(int i = 0; i < x.size(); i++) {
+        if(x[i] == TRUE) {
+            counter ++;
+        }
+    }
+return counter;
 }
 
 /*** R
@@ -43,15 +43,17 @@ int count_if(LogicalVector x) {
  * be written as follows. While this function is simple, the
  * downside is that additional functions will have to be
  * written for other logical operators and other types.
+ * Please see the references for the [functional](http://www.cplusplus.com/reference/functional/) and [algorithm](http://www.cplusplus.com/reference/algorithm/)
+ * headers for information regarding `std::less`, `bind2nd`, and `count_if`.
  */
 
-#include <vector>
-#include <functional>
-#include <algorithm>
+#include <vector>        // for std::vector
+#include <functional>    // for std::less and bind2nd
+#include <algorithm>     // for count_if
 
 // [[Rcpp::export]]
 int count_if_lt(std::vector<double> x, int n) {
-	return count_if(x.begin(), x.end(), bind2nd(std::less<double>(), n));
+    return count_if(x.begin(), x.end(), bind2nd(std::less<double>(), n));
 }
 
 /*** R
