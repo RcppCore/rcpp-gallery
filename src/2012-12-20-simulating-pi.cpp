8ada33df75e0a79a17737e850e3b71d7c5071d61
Dirk Eddelbuettel <edd@debian.org>
2014-07-19 16:42:11 -0500

    remove RNGScope from code but mention in text

--- src/2012-12-20-simulating-pi.cpp
+++ src/2012-12-20-simulating-pi.cpp
@@ -47,18 +47,19 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 double piSugar(const int N) {
-  RNGScope scope;		// ensure RNG gets set/reset
-  NumericVector x = runif(N);
-  NumericVector y = runif(N);
-  NumericVector d = sqrt(x*x + y*y);
-  return 4.0 * sum(d < 1.0) / N;
+    NumericVector x = runif(N);
+    NumericVector y = runif(N);
+    NumericVector d = sqrt(x*x + y*y);
+    return 4.0 * sum(d < 1.0) / N;
 }
 
 /**
  * Apart from using types (hey, this is C++) and assuring the RNG gets
  * set and reset, the code is essentially identical.
  *
- * And by using the same RNG, so are the results.
+ * And by using the same RNG, so are the results. Rcpp ensures that
+ * the RNG state is set and reset properly by instantiating an object 
+ * of class `RNGScope`. 
  */
 
 /*** R
